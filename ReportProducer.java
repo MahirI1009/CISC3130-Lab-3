a661b60c5d3b2e45de106a0be4a404f84ff77870
Mahir Imtiyaz <55066252+MahirI1009@users.noreply.github.com>
2020-10-19 12:06:16 -0400

    Update and rename Lab3.java to ReportProducer.java

--- ReportProducer.java
+++ ReportProducer.java
@@ -1,15 +1,22 @@
 import java.util.*;
 import java.io.*;
 
-public class Lab3 {
+/* In this class is the main method, it reads in a CSV file from Spotify's website, if you want to test this code you can use any CSV file from Spotify for their top 200
+ * artists. In this class I am using the CSV class for the top 200 in the US for the week of October 1, 2020. In the main method a Scanner object reads in the file and only
+ * takes the third value per line as that is the value that contains the artist's name. It adds all the names to an ArrayList, then a for loop goes through the ArrayList and
+ * sorts the ArrayList in alphabetical order, then another for loop removes all duplicate names in the ArrayList. Finally, it creates an instance of my custom LinkedList class 
+ * for artists and adds in all artists and then uses my custom printArtists method which uses a PrintWriter object to print the list of artists to an output file, however there
+ * is a method called displayArtists that one can use if they prefer the list be printed to the console. In my program this output file is called "reportOfTop200Artists.txt" */
+
+public class ReportProducer {
 
 public static void main(String[] args) throws Exception {
 		
-	String csv = "regional-us-weekly-latest-2.csv";
+	String csv = "regional-us-weekly-latest.csv";
 	//String to hold file name
 	Scanner sc = new Scanner(new File(csv));
 	//Scanner object to read the csv file
-	PrintWriter outputFile = new PrintWriter("Artists-WeekOf10012020.txt");
+	PrintWriter outputFile = new PrintWriter("reportOfTop200Artists.txt");
 	//printwriter object to print the report to an output file
 		
 	String header = sc.nextLine(); 
@@ -31,7 +38,7 @@ public static void main(String[] args) throws Exception {
             if (remove.charAt(0) == '"')
             	artistName = remove.substring(1, remove.length()-1);
             else artistName = name[name.length-3];
-            /*some of the artist names in the file were surrounded with quotation marks while some didnt, which caused a problem 
+            /* some of the artist names in the file were surrounded with quotation marks while some didnt, which caused a problem 
              * for sorting the list in alphabetical order, so I made sure to check if a name starts with a quotation mark, if it
              * does then the quotation marks are removed from the String, this what the String remove is being used for
              * Secondly, I made sure artistName is assigned the 2nd to last value of every row because some values have commas 
@@ -72,7 +79,7 @@ public static void main(String[] args) throws Exception {
         } //end of for loop
         
         
-        /* I made my own custom Artist node and LinkedList classes for this lab, I am now instantiating my custom LinkedList below and I 
+        /* I made my own custom Artist node and LinkedList classes for this project, I am now instantiating my custom LinkedList below and I 
          * intend to add all the names from the top200 arrayList to my custom LinkedList*/
         
         LinkedListOfArtists listOfArtists = new LinkedListOfArtists();
@@ -88,9 +95,8 @@ public static void main(String[] args) throws Exception {
         } //end of for loop
         
         
-        /*I initially made a displayList method.... but, since the lab requires that it be printed to file, instead of to the console
-         *I instead made a method in my custom LinkedList and Node classes that would print the list to an output file, and it would 
-         *take in the printwriter used to print to the output file as a parameter*/
+        /*I initially made a displayList methodbut, instead of printing to the console, I made a custo, method in my custom LinkedList and Node classes that 
+	 * would print the list to an output file, and it would take in the printwriter used to print to the output file as a parameter*/
         
         listOfArtists.printArtists(outputFile); 
         /*Here I called the method that prints the List to an output file, and Im sending the printwriter as a parameter so that the method
